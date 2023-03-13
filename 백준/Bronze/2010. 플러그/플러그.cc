1e1a1c73ebba96023ad8a6ef64c984052935343a
whisoo98 <71370211+whisoo98@users.noreply.github.com>
2023-03-13 10:02:05 +0900

    [Bronze III] Title: 플러그, Time: 40 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.cc
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include <stdlib.h>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+
+
+using namespace std;
+
+/*struct type_name {
+
+}srt;*/
+
+//vector <type> vec;
+//stack <type> stk;
+//queue <type> q;
+//string str;
+//map <key_type,value_type> m;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int i, j;//for문변수-->가끔식 long으로
+	int n;
+	int sum = 1;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		int temp;
+		scanf("%d", &temp);
+		sum += temp;
+	}
+	printf("%d", sum - n);
+	return 0;
+}
