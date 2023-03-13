4e610e766d5ead3972362303f6fa44af0acb7e23
whisoo98 <71370211+whisoo98@users.noreply.github.com>
2023-03-13 09:49:43 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+	int A,B;
+
+	scanf("%5d %5d", &A, &B);
+
+	//printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	//printf("%d\n", A * B);
+	//printf("%d\n", A / B);
+	//printf("%d\n", A % B);
+
+	return 0;
+}
