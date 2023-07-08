514e963055bdf709ed28698dd2ecb5216fb606a1
whisoo98 <71370211+whisoo98@users.noreply.github.com>
2023-07-08 15:42:04 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <ctime>
+#include <climits>
+#include <tuple>
+#define N 100'005
+#define ll long long
+#define endl "\n"
+#define MID(a,b) (a+b)/2
+
+using namespace std;
+
+
+int dp[N];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int i, j; //for문 변수 -> longlong필요한지 확인
+	int n; cin >> n;
+	dp[1] = -1;
+	dp[2] = 1;
+	dp[3] = -1;
+	dp[4] = 2;
+	dp[5] = 1;
+	dp[6] = 3;
+	dp[7] = 2;
+	dp[8] = 4;
+	for (int i = 9; i <= n; i++) {
+		dp[i] = min(dp[i - 2], dp[i - 5]) + 1;
+	}
+	cout << dp[n] << endl;
+	return 0;
+}
