9e7fefa613e0b78c1bff621d33f0f86d6b898ead
whisoo98 <71370211+whisoo98@users.noreply.github.com>
2023-10-28 19:34:12 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -1,26 +1,66 @@
-#include <stdio.h>
-#include<string>
-#include<iostream>
-/*A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35*/
-//A 65 Z90 0-48 9 - 57
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <ctime>
+#include <climits>
+#include <tuple>
+#define N 50
+#define ll long long
+#define endl "\n"
+#define MID(a,b) (a+b)/2
+
 using namespace std;
-int arr[1000005];
+string num_to_string[N];
+
+
+void preProcess() {
+	for (int i = 0; i < 10; i++) {
+		num_to_string[i] = '0' + i;
+	}
+	for (int i = 10; i < 36; i++) {
+		num_to_string[i] = 'A' + (i - 10);
+	}
+}
+
+string convertBase(int n, int base) {
+	string ret = "";
+	while (n) {
+		int rest = n % base;
+		n /= base;
+		ret += num_to_string[rest];
+	}
+	reverse(ret.begin(), ret.end());
+	return ret;
+}
+
+void Solution(int n, int base) {
+	string s;
+	preProcess();
+	s = convertBase(n, base);
+	cout << s << endl;
+}
+
+
 int main()
 {
-	int n, b;
-	scanf("%d %d", &n, &b);
-	int i = 0;
-	int j;
-	while ( n )
-	{
-		int temp = n % b;
-		if ( temp >= 10 ) temp = temp + 55;
-		else temp = temp + 48;
-		arr[i] = temp;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-		i++;
-		n = n / b;
-	}
-	for ( j=i - 1; j >= 0; j-- ) printf("%c", arr[j]);
+	int i, j; //for문 변수 -> longlong필요한지 확인
+	int n; cin >> n;
+	int base; cin >> base;
+	Solution(n, base);
 	return 0;
 }
